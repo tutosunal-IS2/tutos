a5a385364be2df10b724e080cf2478d27b6d44b9
jaavargasar <jaavargasar@gmail.com>
2016-10-12 19:08:14 -0500

    fixing routes errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,26 +1,29 @@
 Rails.application.routes.draw do
 
 
+  resources :subjects
+  get 'tutorials/index'
+
+  devise_for :admins
   resources :tutoria
   devise_for :tutors
 
-  
-  devise_for :students, 
+
+  devise_for :students,
   	controllers: {
-  		 omniauth_callbacks: "students/omniauth_callbacks" 
-       
+  		 omniauth_callbacks: "students/omniauth_callbacks"
+
   		}
 
   devise_scope :user do delete "logout" => "devise/sessions#destroy" end
  #get 'logout' => 'students/omniauth_callbacks#destroy_student'
-  
+
   get 'entrar' => 'intro#renderEntrar'
   get 'team' => 'about#renderAbout'
   get 'function' => 'services#renderServices'
+  get 'tutos' => 'tutorials#home'
   post "contactUS" => 'pages#contactUS'
+
+  get 'solving' => 'subjects#solve'
   root "pages#index"
-
-
-  
-    
 end
