a82946852676f9de0a96b314921691eb44ed0511
jaavargasar <jaavargasar@gmail.com>
2016-10-12 19:11:23 -0500

    fixing application controller errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception, prepend: true
-  skip_before_filter :verify_authenticity_token 
-  
+  skip_before_filter :verify_authenticity_token
+   def after_sign_in_path_for(resource)
+  	session["tutor_return_to"] || tutos_path
+   end
 
    def new_session_path(scope)
     new_student_session_path
