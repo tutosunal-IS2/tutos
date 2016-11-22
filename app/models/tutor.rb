c9671cebeb8644fb0485df563904ed011a98ba17
Adiossussan980 <cjmedinav@unal.edu.co>
2016-11-22 17:59:35 -0500

    Validacion correo

--- app/models/tutor.rb
+++ app/models/tutor.rb
@@ -1,6 +1,8 @@
 class Tutor < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+(@gmail|@yahoo|@hotmail)\.com\z/,
+                  message: "must be a valid email" }
   mount_uploader :avatar, AvatarUploader
   has_many :likes_tutors_by_subjects
   has_many :subjects, through: :likes_tutors_by_subjects, dependent: :destroy
